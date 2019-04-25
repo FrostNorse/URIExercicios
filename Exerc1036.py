31370f75309884b2491e639c565b563e1f8c1f0b
FrostNorse <49586777+FrostNorse@users.noreply.github.com>
2019-04-25 12:04:41 -0300

    Update Exerc1036.py

--- Exerc1036.py
+++ Exerc1036.py
@@ -1,2 +1,13 @@
-a, b, c = map(int, input().split())
-delta = b * b
+import math
+a, b, c = map(float, input().split())
+if a != 0:
+    delta = b * b - (4 * a *c)
+    if delta < 0:
+        print("Impossivel calcular")
+    else:
+        x1 = (-b + math.sqrt(delta)) / (2*a)
+        x2 = (-b - math.sqrt(delta)) / (2*a)
+        print("R1 = %.5F" % x1)
+        print("R2 = %.5F" % x2)
+else:
+    print("Impossivel calcular")
