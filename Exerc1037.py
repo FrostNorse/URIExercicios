5eaaf9b35c5037b5e962d26fc1857d714487de4f
FrostNorse <49586777+FrostNorse@users.noreply.github.com>
2019-04-25 12:17:07 -0300

    Add files via upload

---
+++ Exerc1037.py
@@ -0,0 +1,13 @@
+num = float(input())
+if num < 0 or num >100:
+    print("Fora de intervalo")
+else:
+    num = num/25
+    if num <= 1:
+        print("Intervalo [0,25]")
+    elif num <= 2:
+        print("Intervalo [25,50]")
+    elif num <= 3:
+        print("Intervalo [50,75]")
+    else:
+        print("Intervalo [75,100]")
