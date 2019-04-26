eed3625a159ac65728ef9288f061b95832c28cc8
FrostNorse <49586777+FrostNorse@users.noreply.github.com>
2019-04-25 22:32:32 -0300

    Update Exerc1030.py

--- Exerc1030.py
+++ Exerc1030.py
@@ -1,27 +1,13 @@
-case = int(input())
-for j in range(0, case):
-    quantidadeSoldado, pulo = map(int, input().split())
-    soldados = []
-    soldadosvivos = quantidadeSoldado
-    soldadoatual = 1
-    puloatual = 0
-    for i in range(0, quantidadeSoldado):
-        soldados.append(i + 1)
-    while soldadosvivos != 1:
+quantidadeSoldado, pulo = map(int, input().split())
+soldado = []
+soldadoAtual = 0
+for i in range(quantidadeSoldado):
+    soldado.append(i+1)
+while len(soldado) > 1:
+    soldadoAtual += pulo-1
+    if soldadoAtual > len(soldado):
+        soldadoAtual %= len(soldado)
+    soldado.remove(soldado[soldadoAtual])
+print(soldado[0])
 
-        if soldadoatual > quantidadeSoldado:
-            soldadoatual -= quantidadeSoldado
 
-        if soldados[soldadoatual - 1] > 0:
-            puloatual += 1
-
-        if puloatual == pulo:
-            soldados[soldadoatual - 1] = 0
-            puloatual = 0
-            soldadosvivos -= 1
-
-        soldadoatual += 1
-
-    for i in soldados:
-        if i > 0:
-            print("Case {}:".format(j+1), i)
