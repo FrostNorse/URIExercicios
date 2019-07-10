c16b5723ded240905dc48d839d311db35500a4b4
FrostNorse <49586777+FrostNorse@users.noreply.github.com>
2019-07-10 15:12:05 -0300

    Update Exerc1138.py

--- Exerc1138.py
+++ Exerc1138.py
@@ -1,10 +1,12 @@
-inicio, fim = map(int, input().split())
-num = inicio
-digitos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-while num <= fim:
-    numero = str(num)
-    for i in numero:
-        digitos[int(i)] += 1
-    num += 1
-print(str(digitos).strip("[]"))
-
+while True:
+    inicio, fim = map(int, input().split())
+    if inicio == 0 and fim == 0:
+        break
+    num = inicio
+    digitos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    while num <= fim:
+        numero = str(num)
+        for i in numero:
+            digitos[int(i)] += 1
+        num += 1
+    print(str(digitos).strip("[]").replace(",", ""))
